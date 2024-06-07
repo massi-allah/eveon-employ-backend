1613a8256be0db77ac3527b77eb067b38989979c
massiullah <messi2010arz@gmail.com>
2024-06-07 09:11:04 +0430

    Update 1

--- employbackend/settings.py
+++ employbackend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c(lg0^8d=ehxc*@io1kkd3b4q%py==w^jbzlhk$kr%%nog8th7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -94,14 +94,7 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
 
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
